2b3ffe4d91f819aed9289f9a9b45cf93440d50ba
mateusntn <mateusantoniodev@gmail.com>
2021-02-24 20:27:13 -0300

    Adds the title and favicon to the page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,15 @@ import { CompletedChallanges } from "../components/CompletedChallenges"
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 
+import Head from "next/head";
 import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | Renew</title>
+      </Head>
       <ExperienceBar />
       <section>
         <div>
